8fa248e0eb20e36c9cdbdf5f218961561f110532
ClareChu <1062186165@qq.com>
2020-08-11 15:44:32 +0800

    es set auth

--- starter/es/client.go
+++ starter/es/client.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"github.com/olivere/elastic/v6"
 	"hidevops.io/hiboot/pkg/at"
+	"time"
 )
 
 type Properties struct {
 	at.ConfigurationProperties `value:"es"`
 	Port                       int    `json:"port" default:"5672"`
 	Host                       string `json:"host" default:"127.0.0.1"`
+	Username                   string `json:"username" default:"elastic"`
+	Password                   string `json:"password" default:"password"`
 }
 
 type Client struct {
@@ -22,7 +25,12 @@ func newClient() (client *Client) {
 
 func (c *Client) Connect(p *Properties) (err error) {
 	esUrl := fmt.Sprintf("http://%s:%d", p.Host, p.Port)
-	client, err := elastic.NewSimpleClient(elastic.SetURL(esUrl))
+	client, err := elastic.NewSimpleClient(
+		elastic.SetURL(esUrl),
+		elastic.SetHealthcheckInterval(10*time.Second),
+		elastic.SetURL(esUrl),
+		elastic.SetBasicAuth(p.Username, p.Password),
+		)
 	if err != nil {
 		return
 	}
